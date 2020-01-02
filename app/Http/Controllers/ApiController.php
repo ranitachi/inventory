129dc131665f0e107b1ba13be227d4f5231a6644
ranitachi <fachran.nazarullah@gmail.com>
2020-01-02 18:00:06 +0700

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Product_Masuk;
+use App\Product;
 class ApiController extends Controller
 {
     public function getuserbyname(Request $request)
@@ -39,7 +41,7 @@ class ApiController extends Controller
         {
             $insert=new User;
             $insert->name=$nama;
-            $insert->email=str_replace('Email : ','',$email);
+            $insert->email=strtolower(str_replace('Email : ','',$email));
             $insert->telepon=str_replace('No.HP : ','',$telepon);
             $insert->role='staff';
             $insert->password=bcrypt($telepon);
@@ -55,5 +57,64 @@ class ApiController extends Controller
                 'status'=>404,
                 'data' => array('message'=>'Terjadi Kesalahan')
             ]);
+    }
+    
+    public function store_product_in(Request $request)
+    {
+        $nama=$request->nama;
+        $product_number = $request->product_number;
+        $product_name = $request->product_name;
+        $vendor = $request->vendor;
+        $deskripsi = $request->deskripsi;
+        $order_ref = $request->order_ref;
+        $jumlah = (int)$request->jumlah;
+
+        $cek=User::where('name',$nama)->first();
+        if($cek)
+        {
+            $cekproduct=Product::where('product_number',$product_number)->first();
+            // return $cekproduct;
+            if($cekproduct)
+            {
+                $cekproduct->qty=$cekproduct->qty+$jumlah;
+                $cekproduct->save();
+
+                $product_id=$cekproduct->id;
+            }
+            else
+            {
+                $prd=new Product;
+                $prd->category_id=0;      
+                $prd->nama=$product_name;      
+                $prd->vendor=$vendor;      
+                $prd->qty=$jumlah;      
+                $prd->save();      
+                
+                $product_id=$prd->id;
+            }
+
+            $insert=new Product_Masuk;
+            $insert->product_id=$product_id;
+            $insert->supplier_id=0;
+            $insert->qty=$jumlah;
+            $insert->tanggal=date('Y-m-d');
+            $insert->deskripsi=$deskripsi;
+            $insert->order_ref=$order_ref;
+            $insert->received_by=$nama;
+            $insert->save();
+
+
+            return response()->json([
+                'status'=>200,
+                'data' => array('message'=>'Data Anda Berhasil Di Simpan')
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'status'=>405,
+                'data' => array('message'=>'Akun Anda Belum Terdaftar, Silahkan Daftar Terlebih Dahulu dengan ketik ')
+            ]);
+        }
     }
 }
